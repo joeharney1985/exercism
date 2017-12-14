581250438e651cbab417faff41e89d7dc0be965c
harney <harney@prosensing.com>
2017-12-14 15:14:41 -0500

    fixed intentional bug from previous commit.  The badge updates, but much later than I expected.

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -10,4 +10,4 @@ def to_rna(dna_strand):
     try:
         return ''.join(mapping[c] for c in dna_strand)
     except KeyError as e:
-        raise ValueError('invalid input.')sdf
+        raise ValueError('invalid input.')

188b43af2f3d0f62a7be49db617be59574c1fb4b
harney <harney@prosensing.com>
2017-12-14 15:07:55 -0500

    python3 KeyErrors do not have the key as the messag.

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -10,4 +10,4 @@ def to_rna(dna_strand):
     try:
         return ''.join(mapping[c] for c in dna_strand)
     except KeyError as e:
-        raise ValueError('{} is an invalid input.'.format(e.message))
+        raise ValueError('invalid input.')

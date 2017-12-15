9a35e0c5192a57345c567c37b01b11fdfdad095f
Joe Harney <josephpharney@gmail.com>
2017-12-14 20:49:55 -0500

    added atbash-cipher solution.

---
+++ python/atbash-cipher/atbash_cipher.py
@@ -0,0 +1,17 @@
+import string
+import re
+
+CIPHER = string.maketrans(string.ascii_lowercase, string.ascii_lowercase[::-1])
+remove = string.whitespace + string.punctuation
+
+
+def add_spaces(ciphered_text):
+    return re.sub(r'(.....)', r'\1 ', ciphered_text).strip()
+
+
+def encode(plain_text):
+    return add_spaces(plain_text.lower().translate(CIPHER, remove))
+
+
+def decode(ciphered_text):
+    return ciphered_text.lower().translate(CIPHER, remove)

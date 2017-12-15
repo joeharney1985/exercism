7af274941d9eb2d024bf3230fc0eae7d33e0743b
Joe Harney <josephpharney@gmail.com>
2017-12-15 15:27:50 -0500

    Another solution on exercism.io helped me realize that range concisely calculates the multiples.

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,9 +1,2 @@
-import math
-
-
-def multiples_to(limit, base):
-    return [base * i for i in range(1, int(math.ceil(limit / float(base))))]
-
-
 def sum_of_multiples(limit, bases):
-    return sum(set([m for b in bases for m in multiples_to(limit, b)]))
+    return sum(set([m for b in bases for m in range(b, limit, b)]))

4da06e13bd58028fa88d32d077ca15240cf7d48c
Joe Harney <josephpharney@gmail.com>
2017-12-15 09:48:26 -0500

    Finally mae the code python 2x and 3x compatible (I think...).

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,13 +1,26 @@
 import string
 import re
 try:
-    from string import maketrans
-except ImportError:
-    maketrans = str.maketrans
+    from string import maketrans as mktrans
+except ImportError:  # python3
+    mktrans = str.maketrans
 
 
-CIPHER = maketrans(string.ascii_lowercase, string.ascii_lowercase[::-1])
+def maketrans(_from, to, remove):
+    """ Creates a translation tuple which is compatible with python 2 and 3.
+    When calling str.translate() use the * operator to unpack the tuple.  For
+    python 2 This will unpack the translation table and the list of chars to
+    remove.  For python 3 the list of chars to remove are in the table and the
+    table is the only item in the tuple to unpack.
+    """
+    try:
+        return mktrans(_from, to, remove),
+    except TypeError:
+        return mktrans(_from, to), remove
+
 remove = string.whitespace + string.punctuation
+backwards = string.ascii_lowercase[::-1]
+CIPHER = maketrans(string.ascii_letters, backwards * 2, remove)
 
 
 def add_spaces(ciphered_text):
@@ -15,8 +28,9 @@ def add_spaces(ciphered_text):
 
 
 def encode(plain_text):
-    return add_spaces(plain_text.lower().translate(CIPHER, remove))
+    encoded = plain_text.translate(*CIPHER)
+    return add_spaces(encoded)
 
 
 def decode(ciphered_text):
-    return ciphered_text.lower().translate(CIPHER, remove)
+    return ciphered_text.translate(*CIPHER)
